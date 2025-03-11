8d6f9d4a076e85d154993a6b9c6e53b9834bb3e6
nkrsky <111038995+nkrsky@users.noreply.github.com>
2025-03-11 12:23:09 +0530

    Create utils.py

---
+++ test_folder/inventory_manager/utils.py
@@ -0,0 +1,8 @@
+def validate_input(prompt: str, input_type=int):
+    """Validates user input."""
+    while True:
+        try:
+            value = input_type(input(prompt))
+            return value
+        except ValueError:
+            print("Invalid input. Please try again.")
